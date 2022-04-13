47cbece59239725a05c65d8842df717fe0f16686
Marlytos6 <matheusmhirata5@gmail.com>
2022-04-13 16:55:51 -0300

    site descontinuado

--- teste.js
+++ teste.js
@@ -1,17 +1,15 @@
-//testar codigo
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
       this._mobileMenu = document.querySelector(mobileMenu)
       this._navList = document.querySelector(navList)
       this._navLinks = document.querySelectorAll(navLinks)
-      this._activeClass = "active";
   
       this._handleClick = this.handleClick.bind(this);
     }
   
     handleClick() {
-      this._navList.classList.toggle(this._activeClass)
-      this._navLinks.classList.toggle(this._activeClass)
+      const nav = document.querySelector('nav')
+      nav.classList.toggle("active")
     }
   
     addClickEvent() {
@@ -26,5 +24,5 @@ class MobileNavbar {
     }
   }
   
-  const Mobile = new MobileNavbar('#btn_menu', '#menu_list', '#menu_list li')
+  const Mobile = new MobileNavbar('#btn_menu', '#menu_list', '#menu_list li',)
   Mobile.init()
