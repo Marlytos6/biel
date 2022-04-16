0a3941b6b6e45281c3947b6acac1ffe85fff9de9
Marlytos6 <matheusmhirata5@gmail.com>
2022-04-16 11:42:17 -0300

    teste do modelo de nav bar

--- teste.js
+++ teste.js
@@ -1,16 +1,25 @@
 class MobileNavbar {
   constructor(mobileMenu, navList, navLinks) {
+    /* seletores */
     this._mobileMenu = document.querySelector(mobileMenu)
     this._navList = document.querySelector(navList)
     this._navLinks = document.querySelectorAll(navLinks)
 
+    /* define o 'this' como o elemento do html e nao o objeto em JavaScript */
     this._handleClick = this.handleClick.bind(this);
   }
 
   handleClick(event) {
-    if (event.type === 'touchstart') event.preventDefault();
     const nav = document.querySelector('nav')
+    const active = nav.classList.contains("active")
+    if (event.type === 'touchstart') event.preventDefault();
     nav.classList.toggle("active")
+    event.currentTarget.setAttribute('aria-expanded', !active);
+    if (!active) {
+      event.currentTarget.setAttribute('aria-label', 'fechar menu')
+    } else {
+      event.currentTarget.setAttribute('aria-label', 'abrir menu')
+    }
   }
 
   addClickEvent() {
