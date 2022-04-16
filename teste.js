f72b2600baa86d2b0658afe277369a9f5b7cb82b
Marlytos6 <matheusmhirata5@gmail.com>
2022-04-16 00:19:33 -0300

    testes apenas

--- teste.js
+++ teste.js
@@ -1,28 +1,30 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this._mobileMenu = document.querySelector(mobileMenu)
-      this._navList = document.querySelector(navList)
-      this._navLinks = document.querySelectorAll(navLinks)
-  
-      this._handleClick = this.handleClick.bind(this);
-    }
-  
-    handleClick() {
-      const nav = document.querySelector('nav')
-      nav.classList.toggle("active")
-    }
-  
-    addClickEvent() {
-      this._mobileMenu.addEventListener('click', this.handleClick)
-    }
-  
-    init() {
-      if (this._mobileMenu) {
-        this.addClickEvent()
-      }
-      return this
-    }
+  constructor(mobileMenu, navList, navLinks) {
+    this._mobileMenu = document.querySelector(mobileMenu)
+    this._navList = document.querySelector(navList)
+    this._navLinks = document.querySelectorAll(navLinks)
+
+    this._handleClick = this.handleClick.bind(this);
   }
-  
-  const Mobile = new MobileNavbar('#btn_menu', '#menu_list', '#menu_list li',)
-  Mobile.init()
+
+  handleClick(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.querySelector('nav')
+    nav.classList.toggle("active")
+  }
+
+  addClickEvent() {
+    this._mobileMenu.addEventListener('click', this.handleClick)
+    this._mobileMenu.addEventListener('touchstart', this.handleClick)
+  }
+
+  init() {
+    if (this._mobileMenu) {
+      this.addClickEvent()
+    }
+    return this
+  }
+}
+
+const Mobile = new MobileNavbar('#btn_menu', '#menu_list', '#menu_list li',)
+Mobile.init()
