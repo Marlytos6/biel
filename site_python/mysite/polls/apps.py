a31b69e0eac10cd667839668f70b87709ddc3ac2
Marlytos6 <matheusmhirata5@gmail.com>
2022-04-22 04:33:42 -0300

    erro
    esta pedindo um valor inteiro como retorno no views

--- site_python/mysite/polls/apps.py
+++ site_python/mysite/polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PollysConfig(AppConfig):
+class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pollys'
+    name = 'polls'
